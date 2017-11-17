c0f0569b56c397bbb3d02bee25f5286729ff0b6c
cuishixiang <cuishixiang@maizijf.com>
2017-11-17 18:31:54 +0800

    test:null相关测试

---
+++ codetest/src/test/java/com/cui/code/test/NullTest.java
@@ -0,0 +1,16 @@
+package com.cui.code.test;
+
+import org.junit.Test;
+
+/**
+ * null相关测试
+ * Created by cuishixiang on 2017-11-17.
+ */
+public class NullTest {
+
+    @Test
+    public void testCompareZero() {
+        Integer num = null;
+        System.out.println(num == 0);
+    }
+}
