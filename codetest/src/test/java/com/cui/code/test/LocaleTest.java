b905cc82986968b1c0197aea27963247aa182cca
cuishixiang <cuishixiang@mobike.com>
2018-09-19 12:09:14 +0800

    ResourceBundle、Locale测试

--- codetest/src/test/java/com/cui/code/test/LocaleTest.java
+++ codetest/src/test/java/com/cui/code/test/LocaleTest.java
@@ -6,6 +6,7 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 /**
+ * 国际化测试：ResourceBundle、Locale、MessageFormat
  * Created by cuishixiang on 2017-09-10.
  */
 public class LocaleTest {
@@ -24,13 +25,31 @@ public class LocaleTest {
 
         Locale defaultLocale = Locale.getDefault();
         System.out.println(defaultLocale.getDisplayCountry() + "=" + defaultLocale.getCountry() + "   " + defaultLocale.getDisplayLanguage() + "=" + defaultLocale.getLanguage());
+
+        Locale zh = Locale.forLanguageTag("zh");
+        System.out.println(zh.getDisplayCountry() + "=" + zh.getCountry());
+
+        Locale china = Locale.CHINA;
+        System.out.println(china.getDisplayCountry() + "=" + china.getCountry() + "   " + china.getDisplayLanguage() + "=" + china.getLanguage());
+
+        Locale chinese = Locale.CHINESE;
+        System.out.println(chinese.getDisplayCountry() + "=" + chinese.getCountry() + "   " + chinese.getDisplayLanguage() + "=" + chinese.getLanguage());
     }
 
     @Test
     public void testResourceBundle() {
-//        Locale locale = Locale.getDefault(Locale.Category.FORMAT);
-        Locale locale = new Locale("en", "GB");
+        //Locale locale = Locale.getDefault(Locale.Category.FORMAT);
+        //Locale locale = Locale.getDefault();
+        //Locale locale = new Locale("en", "GB");
+        //Locale locale = new Locale("en", "US");
+        //Locale locale = new Locale("en");
+        Locale locale = new Locale("zh");
+        //测试使用不存在的语言时的场景
+        //Locale locale = new Locale("");
+        //Locale locale = new Locale(null);
+        //Locale locale = new Locale("nono");
         System.out.println(locale.getDisplayCountry() + "=" + locale.getCountry() + "   " + locale.getDisplayLanguage() + "=" + locale.getLanguage());
+        // 加载资源不对？有疑问时请先详细看此方法的注释及源码
         ResourceBundle resourceBundle = ResourceBundle.getBundle("message", locale);
         System.out.println(resourceBundle.getString("001"));
 
