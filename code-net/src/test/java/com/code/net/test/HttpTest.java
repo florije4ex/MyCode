5a5527c14d103986df1b8cac17a84a483f7b467d
cuishixiang <cuishixiang@mobike.com>
2018-12-11 12:48:55 +0800

    net：京津冀旅游年卡景区预约测试

--- code-net/src/test/java/com/code/net/test/HttpTest.java
+++ code-net/src/test/java/com/code/net/test/HttpTest.java
@@ -1,12 +1,21 @@
 package com.code.net.test;
 
 import com.alibaba.fastjson.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.Test;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -21,7 +30,7 @@ public class HttpTest {
 
     private RestTemplate restTemplate = new RestTemplate();
 
-    String url = "https://s.creditcard.ecitic.com/citiccard/lottery-gateway-pay/pointLottery.do";
+    private String url = "https://s.creditcard.ecitic.com/citiccard/lottery-gateway-pay/pointLottery.do";
 
     @Test
     public void testGet() {
@@ -92,5 +101,135 @@ public class HttpTest {
         }
     }
 
+    /**
+     * 京津冀旅游年卡景区预定 测试
+     */
+    @Test
+    public void testBookTicket() {
+        // 天津的相声：9   奥林匹克塔：7
+        String subscribeId = "9";
+        // 预约日期
+        String bookDate = "2018-12-14";
+        // 预登陆后的JSESSIONID
+        String JSESSIONID = "自己登陆后的jsessionid";
+        String cardNo = "填写自己的卡号";
 
+        System.out.println(new Date());
+        int count = 0;
+        while (true) {
+            if (++count % 100 == 0) {
+                System.out.println("retry count：" + count);
+                System.out.println(new Date());
+            }
+            try {
+                String subscribeCalendarId = getSubscribeCalendarId(subscribeId, bookDate, JSESSIONID);
+                if (subscribeCalendarId != null) {
+                    boolean result = lynkBook(subscribeId, subscribeCalendarId, cardNo, JSESSIONID);
+                    if (result) {
+                        System.out.println(count + "：预约成功，退出循环");
+                        System.out.println(new Date());
+                        break;
+                    }
+                }
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 获取可预订日期的id
+     *
+     * @param subscribeId 景区id
+     * @param bookDate    预定日期
+     * @param JSESSIONID  登陆后的JSESSIONID
+     * @return 日期的id
+     */
+    private String getSubscribeCalendarId(String subscribeId, String bookDate, String JSESSIONID) {
+        String tianjinURL = "http://zglynk.com/ITS/itsApp/goSubscribe.action?subscribeId=" + subscribeId;
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(HttpHeaders.COOKIE, "JSESSIONID=" + JSESSIONID);
+        HttpEntity request = new HttpEntity(httpHeaders);
+
+        String responseString = restTemplate.postForObject(tianjinURL, request, String.class);
+        Document document = Jsoup.parse(responseString);
+        Elements tables = document.getElementsByClass("ticket-info mart20");
+        Element table = tables.get(0);
+        Elements trs = table.getElementsByTag("tr");
+        for (Element tr : trs) {
+            Elements tds = tr.getElementsByTag("td");
+            Element date = tds.get(0);
+            if (bookDate.equals(date.text())) {
+                Element bookTd = tds.get(2);
+                String bookText = bookTd.text();
+                if (bookText.startsWith("可预约")) {
+                    Elements input = bookTd.getElementsByTag("input");
+                    return input.attr("value");
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 京津冀旅游年卡景区预约提交
+     */
+    private boolean lynkBook(String subscribeId, String subscribeCalendarId, String cardNo, String JSESSIONID) {
+        Map<String, String> statusMap = new HashMap<>();
+        statusMap.put("1", "预约成功");
+        statusMap.put("2", "预约失败，请重试！");
+        statusMap.put("3", "超预约规定次数");
+        statusMap.put("4", "卡不在允许预约范围内");
+        statusMap.put("5", "卡不在允许预约范围内");
+        statusMap.put("6", "超过总次数，当天景区预约已满");
+
+        String bookURL = "http://zglynk.com/ITS/itsApp/saveUserSubscribeInfo.action";
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Host", "zglynk.com");
+        httpHeaders.add("Pragma", "no-cache");
+        httpHeaders.add("Cache-Control", "max-age=0");
+        httpHeaders.add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,image/wxpic,image/sharpp,image/apng,image/tpg,*/*;q=0.8");
+        httpHeaders.add("Origin", "http://zglynk.com");
+        httpHeaders.add("User-Agent", "Mozilla/5.0 (Linux; Android 8.0; MI 6 Build/OPR1.170623.027; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.132 MQQBrowser/6.2 TBS/044403 Mobile Safari/537.36 MMWEBID/1085 MicroMessenger/6.7.3.1360(0x2607033A) NetType/WIFI Language/zh_CN Process/tools");
+        httpHeaders.add("Referer", "http://zglynk.com/ITS/itsApp/goSubscribe.action?subscribeId=" + subscribeId);
+        httpHeaders.add("Accept-Language", "zh-CN,en-US;q=0.8");
+        httpHeaders.add("Cookie", "JSESSIONID=" + JSESSIONID);
+
+        MultiValueMap<String, String> parameter = new LinkedMultiValueMap<>();
+        parameter.add("subscribeId", subscribeId);
+        parameter.add("subscribeCalendarId", subscribeCalendarId);
+        parameter.add("cardNo_792826", cardNo);
+        parameter.add("cardType_792826", "1");
+        parameter.add("cardId", "792826#" + cardNo);
+
+        HttpEntity<Object> request = new HttpEntity<>(parameter, httpHeaders);
+
+        int count = 0;
+        for (int i = 0; i < 3; i++) {
+            try {
+                String responseBody = restTemplate.postForObject(bookURL, request, String.class);
+                JSONObject jsonObject = JSONObject.parseObject(responseBody);
+                if ("1".equals(jsonObject.getString("status"))) {
+                    return true;
+                } else {
+                    System.out.println("fail：" + ++count + "——" + responseBody);
+                    System.out.println(statusMap.getOrDefault(jsonObject.getString("status"), "预约失败，请重试！"));
+                }
+                Thread.sleep(100);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+
+        // 成功：
+        //"{\n" +
+        //        "\t\"status\": \"1\",\n" +
+        //        "\t\"message\": \"成功\",\n" +
+        //        "\t\"id\": \"76198\"\n" +
+        //        "}"
+    }
 }
