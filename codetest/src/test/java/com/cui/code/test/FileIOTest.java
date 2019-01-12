9e46f2c9fde65b3afe207ceb7b7ab384bb13b1a0
cuishixiang <cuishixiang@mobike.com>
2019-01-12 14:32:08 +0800

    base：remove unused method

--- codetest/src/test/java/com/cui/code/test/FileIOTest.java
+++ codetest/src/test/java/com/cui/code/test/FileIOTest.java
@@ -1,7 +1,6 @@
 package com.cui.code.test;
 
 import com.cui.code.test.dto.GaodeResultDTO;
-import com.mobike.pandora.client.model.result.Result;
 import org.junit.Test;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,13 +9,11 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.MessageFormat;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * 文件以及IO测试
@@ -46,59 +43,6 @@ public class FileIOTest {
         System.out.println("separator：" + File.separatorChar);
         System.out.println("pathSeparator：" + File.pathSeparator);
         System.out.println(System.getProperty("user.dir"));
-    }
-
-    /**
-     * 发券-营销中心
-     */
-    @Test
-    public void testSendCoupon() {
-        long timeMillis = System.currentTimeMillis();
-        System.out.println("begin time：" + new Date());
-        RestTemplate restTemplate = new RestTemplate();
-        Path path = Paths.get("/Users/cuiswing/Documents/骑行挑战/六个核桃", "六个核桃骑行挑战level2中奖用户.txt");
-        String level1ActivityId = "act_1527665302044582881";
-        String level2ActivityId = "act_1527665524922842583";
-        try {
-            Stream<String> lines = Files.lines(path);
-            //System.out.println(lines.count());
-            int successNum = 0;
-            Integer failNum = 0;
-            List<String> userIds = lines.collect(Collectors.toList());
-            List<String> successList = new ArrayList<>(700);
-            List<String> failList = new ArrayList<>();
-            //pandora-coupon 服务
-            List<String> urls = new ArrayList<>();
-            urls.add("http://10.30.111.142:9422");
-            urls.add("http://10.30.118.144:9422");
-            urls.add("http://10.30.104.141:9422");
-            urls.add("http://10.30.154.134:9422");
-            urls.add("http://10.30.156.129:9422");
-            urls.add("http://10.30.159.130:9422");
-
-            for (int i = 0; i < userIds.size(); i++) {
-                String url = urls.get(i % 6);
-                String userId = userIds.get(i);
-
-                Result result = restTemplate.getForObject(url + "/couponv2/receivebyactivity?activityId={activityId}&userId={userId}", Result.class, level2ActivityId, userId);
-                if (result.getCode().equals("0")) {
-                    successNum++;
-                    successList.add(userId);
-                } else {
-                    failNum++;
-                    failList.add(userId);
-                }
-            }
-            System.out.println("totalNum：" + userIds.size());
-            System.out.println("successNum：" + successNum);
-            System.out.println("failNum：" + failNum);
-            System.out.println("failed：" + failList);
-            System.out.println("end time：" + new Date());
-            System.out.println("cost TimeMillis：" + (System.currentTimeMillis() - timeMillis));
-        } catch (IOException e) {
-            System.out.println("文件读取失败");
-            e.printStackTrace();
-        }
     }
 
     /**
