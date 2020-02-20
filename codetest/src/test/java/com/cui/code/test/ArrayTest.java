73c1e200f92080c97b587fb8a9f9bc584bea7dd6
CUI <hh-csq@163.com>
2020-02-20 22:37:52 +0800

    base：ArrayList#toArray(java.lang.Object[]) 方法测试

--- codetest/src/test/java/com/cui/code/test/ArrayTest.java
+++ codetest/src/test/java/com/cui/code/test/ArrayTest.java
@@ -3,6 +3,7 @@ package com.cui.code.test;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * 数组相关测试
@@ -24,5 +25,33 @@ public class ArrayTest {
         System.out.println(ids2);
 
         System.out.println(Arrays.toString(ids));
+    }
+
+    /**
+     * 数组复制功能测试：
+     * java.util.ArrayList#toArray(java.lang.Object[])
+     * 当参数的长度小于 list 长度时，返回的是新的对象；
+     * 当参数长度>= list 长度时，直接修改的入参数据
+     * 最终的复制功能是由native 方法实现的： java.lang.System#arraycopy(java.lang.Object, int, java.lang.Object, int, int)
+     */
+    @Test
+    public void testArrayCopy() {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4);
+        System.out.println(list);
+
+        Object[] objects = list.toArray();
+        System.out.println(objects);
+
+        Integer[] integers1 = new Integer[0];
+        Integer[] copyIntegers = list.toArray(integers1);
+        System.out.println(integers1);
+        System.out.println(copyIntegers);
+        System.out.println(integers1 == copyIntegers);
+
+        Integer[] integers2 = new Integer[list.size()];
+        Integer[] copyIntegers2 = list.toArray(integers2);
+        System.out.println(integers2);
+        System.out.println(copyIntegers2);
+        System.out.println(integers2 == copyIntegers2);
     }
 }
